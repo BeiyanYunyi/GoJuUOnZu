aa1b7a20bb4f9520654a00d589cbb126f6dcd3cf
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-03-18 21:47:16 +0800

    ✨ Add GitHub Icon

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -9,9 +9,16 @@
     <router-link to="/test">
       <app-card hoverable>Test</app-card>
     </router-link>
+    <a href="https://github.com/lixiang810/GoJuUOnZu">
+      <app-card hoverable>
+        <v-icon size="1rem"> <git-hub-icon /> </v-icon>
+      </app-card>
+    </a>
   </div>
 </template>
 <script lang="ts" setup>
+import GitHubIcon from '@vicons/fa/Github';
+import { Icon as VIcon } from '@vicons/utils';
 import { RouterLink } from 'vue-router';
 import AppCard from './AppCard.vue';
 </script>
