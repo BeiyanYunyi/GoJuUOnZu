9b4d97eb08109fa2e41594b08fe0397deb042750
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-03-18 19:34:55 +0800

    ✨ Improve message display

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -16,5 +16,6 @@ defineProps<{ hoverable?: boolean }>();
 }
 .appCardHoverable:hover {
   box-shadow: 0.2rem 0.2rem 1rem #2c2c2c, -0.2rem -0.2rem 1rem #090909;
+  transition: all 0.25s cubic-bezier(0.075, 0.82, 0.165, 1);
 }
 </style>
--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -7,11 +7,31 @@
       <app-card>
         <h3 class="text-3xl">{{ katakana ? target[1] : target[0] }}</h3>
       </app-card>
+      <transition
+        name="animate"
+        @after-enter="hide"
+        @after-leave="
+          () => {
+            showPlaceHolder = true;
+            target = random();
+          }
+        "
+      >
+        <p v-if="result === 'Correct!'">
+          <v-icon size="1rem" color="green"><check-icon /></v-icon>&emsp;Correct!
+        </p>
+      </transition>
+      <transition name="animate" @after-enter="hide" @after-leave="showPlaceHolder = true">
+        <p v-if="result && result !== 'Correct!'">
+          <v-icon size="1rem" color="red"><times-icon /></v-icon>&emsp;{{ result }}
+        </p>
+      </transition>
+      <p v-if="showPlaceHolder" style="opacity: 0">&emsp;</p>
       <form class="flex flex-col items-center gap-2" @submit="handleSubmit">
         <input v-model="answer" style="background-color: rgba(0, 0, 0, 0.9)" />
         <div class="flex flex-row justify-center gap-4">
-          <button type="submit">
-            <app-card hoverable>Submit</app-card>
+          <button type="submit" :disabled="!showPlaceHolder">
+            <app-card :hoverable="showPlaceHolder">Submit</app-card>
           </button>
           <button type="button" @click="handleRefresh">
             <app-card hoverable>Refresh</app-card>
@@ -25,6 +45,9 @@
   </div>
 </template>
 <script lang="ts" setup>
+import CheckIcon from '@vicons/fa/Check';
+import TimesIcon from '@vicons/fa/Times';
+import { Icon as VIcon } from '@vicons/utils';
 import { ref } from 'vue';
 import AppCard from '../components/AppCard.vue';
 import { flattenedGoJuUOn } from '../goJuUOn';
@@ -32,23 +55,57 @@ import { flattenedGoJuUOn } from '../goJuUOn';
 const random = () => flattenedGoJuUOn[Math.floor(Math.random() * flattenedGoJuUOn.length)];
 const target = ref(random());
 const answer = ref('');
+const result = ref('');
 const katakana = ref(false);
+const showPlaceHolder = ref(true);
 const handleSubmit = (e: Event) => {
   e.preventDefault();
+  showPlaceHolder.value = false;
   if (answer.value === target.value[2]) {
-    alert('Correct!');
-    target.value = random();
+    result.value = 'Correct!';
   } else {
-    alert(`Wrong! Correct:${target.value[2]}`);
+    result.value = `Wrong! Correct: ${target.value[2]}`;
   }
+  answer.value = '';
 };
 const handleRefresh = (e: MouseEvent) => {
   e.preventDefault();
+  result.value = '';
   target.value = random();
 };
 const handleSwitch = (e: MouseEvent) => {
   e.preventDefault();
   katakana.value = !katakana.value;
 };
+const hide = () => {
+  setTimeout(() => {
+    result.value = '';
+  }, 1000);
+};
 </script>
-<style module></style>
+<style scoped>
+@keyframes show {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@keyframes hide {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+.animate-enter-active {
+  animation-name: show;
+  animation-duration: 1s;
+}
+.animate-leave-active {
+  animation-name: hide;
+  animation-duration: 1s;
+}
+</style>
